a0a57d8f90a4d80a4ba4d501d52dc6899c6cf97e
PedroMenezes296 <barananomenezespedro@gmail.com>
2026-05-07 08:44:07 -0300

    feat(cache.h): modificado as definições da cache L1

--- include/cache.h
+++ include/cache.h
@@ -3,15 +3,18 @@
 
 #include <stdint.h>
 
-// Estrutura para guardar os resultados lógicos da simulação 
 typedef struct {
     int hits;
     int misses;
     int acessos_totais;
 } CacheStats;
 
-// Definições básicas para iniciar os testes (podem ser parametrizadas depois)
-#define NUM_VIAS 4
-#define NUM_CONJUNTOS 16 
+// --- CONFIGURAÇÕES DA CACHE L1 (Baseado na Especificação) ---
+#define CACHE_CAPACITY_BYTES 4096 // 4 KB
+#define BLOCK_SIZE_BYTES     32   // 32 Bytes
+#define NUM_VIAS             2    // Associatividade: 2-vias
+
+// O compilador calcula os conjuntos automaticamente para nós!
+#define NUM_CONJUNTOS        (CACHE_CAPACITY_BYTES / (BLOCK_SIZE_BYTES * NUM_VIAS))
 
 #endif
