83ada6f26016158ef310dd414ec64b6b064cb42e
PedroMenezes296 <barananomenezespedro@gmail.com>
2026-05-12 12:55:35 -0300

    feat(cache.h): adicionado configurações da cache l2

--- include/cache.h
+++ include/cache.h
@@ -13,8 +13,13 @@ typedef struct {
 #define CACHE_CAPACITY_BYTES 4096 // 4 KB
 #define BLOCK_SIZE_BYTES     32   // 32 Bytes
 #define NUM_VIAS             2    // Associatividade: 2-vias
-
 // O compilador calcula os conjuntos automaticamente para nós!
 #define NUM_CONJUNTOS        (CACHE_CAPACITY_BYTES / (BLOCK_SIZE_BYTES * NUM_VIAS))
+
+// --- CONFIGURAÇÕES DA CACHE L2 (Unificada) ---
+#define L2_CAPACITY_BYTES 32768 // 32 KB
+#define L2_BLOCK_SIZE     64    // 64 Bytes
+#define L2_NUM_VIAS       8     // 8-vias
+#define L2_NUM_CONJUNTOS  (L2_CAPACITY_BYTES / (L2_BLOCK_SIZE * L2_NUM_VIAS))
 
 #endif
